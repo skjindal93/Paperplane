4b5d0b7bfc8d1161636bf07b330dfe074ab5b155
Shivanker Goel <shivanker.goel@gmail.com>
2013-11-06 00:38:19 +0530

    reverted to ppm

--- image.cpp
+++ image.cpp
@@ -1,5 +1,5 @@
 //
-//  image.cpp
+//  ppm.cpp
 //  PaperPlane
 //
 //  Created by Shivanker Goel on 19/10/13.
@@ -22,91 +22,108 @@ Image::~Image()	{
 	delete[] pixel;
 }
 
-Image* ReadBMP(const char* filename)
-{
-    FILE* f = fopen(filename, "rb");
+
+Image* readP6(const char* file)	{
+	FILE *img;
+	img = fopen(file, "rb");
+	char buff[16];
 	
-    if(f == NULL)
-        throw "Argument Exception";
+	fgets(buff, sizeof(buff), img);
+	//check the image format
+    if (buff[0] != 'P' || buff[1] != '6') {
+		cerr << "File not in the standard P6 format." << endl;
+		return NULL;
+	}
 	
-    unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+	//check for comments
+    int c = getc(img);
+    while (c == '#') {
+#pragma GCC diagnostic ignored "-Wempty-body"
+    	while (getc(img) != '\n');
+        c = getc(img);
+    }
+	ungetc(c, img);
 	
-    // extract image height and width from header
-    int w = *(int*)&info[18];
-    int h = *(int*)&info[22];
-	
-    cout << endl;
-	
-    int row_padded = (w*3 + 3) & (~3);
-    unsigned char* data = new unsigned char[row_padded];
-    unsigned char tmp;
-	
-    for(int i = 0; i < h; i++)
-    {
-        fread(data, sizeof(unsigned char), row_padded, f);
-        for(int j = 0; j < w*3; j += 3)
-        {
-            // Convert (B, G, R) to (R, G, B)
-            tmp = data[j];
-            data[j] = data[j+2];
-            data[j+2] = tmp;
-			
-            cout << "R: "<< (int)data[j] << " G: " << (int)data[j+1]<< " B: " << (int)data[j+2]<< endl;
-        }
+	int w, h;
+	//read image size information
+    if (fscanf(img, "%d %d", &w, &h) != 2) {
+        cerr << "Invalid size for image." << endl;
+        return NULL;
     }
 	
-    fclose(f);
+	int alpha = 0;
+	
+    //read rgb component
+    if (fscanf(img, "%d", &alpha) != 1 || alpha > 255 || alpha < 0) {
+        cerr << "Invalid RGB component for image. Expected an integer in the range 0 to 255.";
+        return NULL;
+    }
+    while (fgetc(img) != '\n') ;
 	
 	Image *obj;
 	obj = new Image(w, h);
 	
+	//read pixel data from file
+	unsigned char *data;
+	data = new unsigned char[w * 3 * h + 5];
+    if (fread(data, 3 * w, h, img) != h)
+        cerr << "Could not read the whole image file! Trying to proceed.";
+    fclose(img);
+	
     for(int i = 0; i < w; ++i)
     	for(int j = 0; j < h; ++j)	{
-    		obj->pixel[i][j][0] = (double)data[3 * (i + w * j) + 0] / 255.0;
-    		obj->pixel[i][j][1] = (double)data[3 * (i + w * j) + 1] / 255.0;
-    		obj->pixel[i][j][2] = (double)data[3 * (i + w * j) + 2] / 255.0;
+    		obj->pixel[i][j][0] = (double)data[3 * (i + w * j) + 0] / (double)alpha;
+    		obj->pixel[i][j][1] = (double)data[3 * (i + w * j) + 1] / (double)alpha;
+    		obj->pixel[i][j][2] = (double)data[3 * (i + w * j) + 2] / (double)alpha;
     	}
+    delete[] data;
 	
-	delete[] data;
-
-    return obj;
+	return obj;
 }
 
 GLuint loadTexture(const char* file)	{
-    FILE* f = fopen(file, "rb");
+    FILE * img;
+	img = fopen(file, "rb");
+	char buff[16];
 	
-    if(f == NULL)
-        throw "Argument Exception";
+	fgets(buff, sizeof(buff), img);
+	//check the image format
+    if (buff[0] != 'P' || buff[1] != '6') {
+		cerr << "Texture file not in the standard P6 format." << endl;
+		return 0;
+	}
 	
-    unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+	//check for comments
+    int c = getc(img);
+    while (c == '#') {
+#pragma GCC diagnostic ignored "-Wempty-body"
+    	while (getc(img) != '\n');
+        c = getc(img);
+    }
+	ungetc(c, img);
 	
-    // extract image height and width from header
-    int w = *(int*)&info[18];
-    int h = *(int*)&info[22];
-	
-    cout << endl;
-	
-    int row_padded = (w*3 + 3) & (~3);
-    unsigned char* data = new unsigned char[row_padded];
-    unsigned char tmp;
-	
-    for(int i = 0; i < h; i++)
-    {
-        fread(data, sizeof(unsigned char), row_padded, f);
-        for(int j = 0; j < w*3; j += 3)
-        {
-            // Convert (B, G, R) to (R, G, B)
-            tmp = data[j];
-            data[j] = data[j+2];
-            data[j+2] = tmp;
-			
-            cout << "R: "<< (int)data[j] << " G: " << (int)data[j+1]<< " B: " << (int)data[j+2]<< endl;
-        }
+	int w, h;
+	//read image size information
+    if (fscanf(img, "%d %d", &w, &h) != 2) {
+        cerr << "Invalid size for texture image." << endl;
+        return 0;
     }
 	
-    fclose(f);
+	int alpha = 0;
+	
+    //read rgb component
+    if (fscanf(img, "%d", &alpha) != 1 || alpha > 255 || alpha < 0) {
+        cerr << "Invalid RGB component for texture image. Expected an integer in the range 0 to 255.";
+        return 0;
+    }
+    while (fgetc(img) != '\n') ;
+	
+	//read pixel data from file
+	unsigned char *data;
+	data = new unsigned char[w * 3 * h + 5];
+    if (fread(data, 3 * w * h, 1, img) != h)
+        cerr << "Could not read the whole texture file! Trying to proceed.";
+    fclose(img);
 	
 	GLuint texture;
 	glGenTextures( 1, &texture );
